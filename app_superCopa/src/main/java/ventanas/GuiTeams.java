2f416d8e10fcd8b819a0a2ab13bc5044bf12baaa
mjosepc <mjose.powell@gmail.com>
2023-11-30 10:52:18 -0300

    commit 2

--- app_superCopa/src/main/java/ventanas/GuiTeams.java
+++ app_superCopa/src/main/java/ventanas/GuiTeams.java
@@ -3,9 +3,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GuiTeams extends JFrame implements ActionListener {
 
+    private Map<String, ImageIcon> Bandera;
     public GuiTeams() {
 
         super("Teams");
@@ -19,17 +24,37 @@ public class GuiTeams extends JFrame implements ActionListener {
         JLabel etiqueta2 = new JLabel("Raking FIFA", SwingConstants.CENTER);
 
         JLabel etiqueta3 = new JLabel("");
+        JLabel etiqueta4 = new JLabel();
 
-        JList lista = new JList<>();
+        Bandera = new HashMap<>();
+
+        JComboBox comboBox = new JComboBox();
+
+        comboBox.addItem("Australia");
+        comboBox.addItem("Chile");
+        comboBox.addItem("Camerun");
+        comboBox.addItem("Alemania");
+
+        comboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String selectedCountry = (String) comboBox.getSelectedItem();
+                    ImageIcon selectedFlag = Bandera.get(selectedCountry);
+                    etiqueta4.setIcon(selectedFlag);
+                }
+            }
+        });
+
 
         JButton boton1 = new JButton("Jugadores");
         JButton boton2 = new JButton("Exit");
 
-
         etiqueta.setBounds(10, 40, 100, 50);
-        lista.setBounds(200, 40, 100, 50);
+        comboBox.setBounds(100, 40, 200, 50);
         etiqueta2.setBounds(10, 100, 100, 50);
         etiqueta3.setBounds(200, 100, 100, 50);
+        etiqueta4.setBounds(300,40,100,50);
 
         boton1.setBounds(100, 200, 150, 40);
         boton2.setBounds(300, 200, 150, 40);
@@ -37,18 +62,23 @@ public class GuiTeams extends JFrame implements ActionListener {
         boton1.addActionListener(this);
         boton2.addActionListener(this);
 
+
         panel.add(etiqueta);
         panel.add(etiqueta2);
         panel.add(etiqueta3);
-        panel.add(lista);
+        panel.add(comboBox);
         panel.add(boton1);
         panel.add(boton2);
-
 
         add(panel);
 
         setVisible(true);
-
+    }
+    private void cargarBanderas() {
+        Bandera.put("Australia", new ImageIcon("C:\\Taller_lab_4\\app_superCopa\\src\\main\\java\\Data/aus.png"));
+        Bandera.put("Chile", new ImageIcon("C:\\Taller_lab_4\\app_superCopa\\src\\main\\java\\Data/chi.png"));
+        Bandera.put("Camerún", new ImageIcon("C:\\Taller_lab_4\\app_superCopa\\src\\main\\java\\Data"/));
+        Bandera.put("Alemania", new ImageIcon("C:\\Taller_lab_4\\app_superCopa\\src\\main\\java\\Data"));
     }
 
     @Override
